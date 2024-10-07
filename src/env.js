d2a26d8dc6daa4178f2182359f97113f1dae2b5e
Fabio <fccoelho7@gmail.com>
2024-10-07 13:29:47 -0300

    chore: make DATABASE_URL required

--- src/env.js
+++ src/env.js
@@ -7,7 +7,7 @@ export const env = createEnv({
    * isn't built with invalid env vars.
    */
   server: {
-    DATABASE_URL: z.string().url().optional(),
+    DATABASE_URL: z.string().url(),
     OPENAI_API_KEY: z.string(),
     NODE_ENV: z
       .enum(["development", "test", "production"])
