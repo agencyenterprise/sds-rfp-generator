6cb692adf1afc4ab6932e53618d50e562a6a34fd
Bruce Cantarim <bruce@cantarim.com>
2024-10-17 18:44:57 -0700

    ✨ feat(app): SDS-1916 Updated mock data with real examples

--- src/data/mockRFPs.ts
+++ src/data/mockRFPs.ts
@@ -2,49 +2,105 @@ import { RFP } from "~/types/types";
 
 const mockRFPs: RFP[] = [
   {
-    id: "1",
-    data: { description: "RFP for web development services" },
+    id: "67113e90d5d021d29631594a",
+    data: {
+      description:
+        "Consultant for Technical Support in Developing Catch-Up Education Program with SEL Components in Ukraine",
+    },
     userId: null,
-    createdAt: new Date("2023-01-01T10:00:00Z"),
-    title: "Web Development RFP",
-    publishedAt: new Date("2023-01-02T10:00:00Z"),
-    updatedAt: new Date("2023-01-03T10:00:00Z"),
+    createdAt: new Date("2024-11-11T00:00:00Z"),
+    title:
+      "Consultant for Technical Support in Developing Catch-Up Education Program with SEL Components in Ukraine",
+    publishedAt: new Date("2024-11-11T00:00:00Z"),
+    updatedAt: new Date("2024-11-11T00:00:00Z"),
   },
   {
-    id: "2",
-    data: { description: "RFP for mobile app development" },
+    id: "67113e4fd5d021d29631593f",
+    data: {
+      description:
+        "Water.org seeks an experienced consultant or firm to provide Water Supply and Sanitation (WSS) financing training for financial institutions in Kyrgyzstan. The training will equip FI staff to support water and ...",
+    },
     userId: null,
-    createdAt: new Date("2023-02-01T10:00:00Z"),
-    title: "Mobile App Development RFP",
-    publishedAt: new Date("2023-02-02T10:00:00Z"),
-    updatedAt: new Date("2023-02-03T10:00:00Z"),
+    createdAt: new Date("2024-12-02T00:00:00Z"),
+    title: "Financing Training",
+    publishedAt: new Date("2024-12-02T00:00:00Z"),
+    updatedAt: new Date("2024-12-02T00:00:00Z"),
   },
   {
-    id: "3",
-    data: { description: "RFP for cloud infrastructure services" },
+    id: "67113d75d5d021d29631591e",
+    data: {
+      description:
+        "The UNOCT, in collaboration with IGAD/ICEPCVE, seeks experienced gender experts from specific African nations to participate in a ToT workshop aimed at integrating gender and intersectional approaches into CT/P...",
+    },
     userId: null,
-    createdAt: new Date("2023-03-01T10:00:00Z"),
-    title: "Cloud Infrastructure RFP",
-    publishedAt: new Date("2023-03-02T10:00:00Z"),
-    updatedAt: new Date("2023-03-03T10:00:00Z"),
+    createdAt: new Date("2024-12-10T00:00:00Z"),
+    title:
+      "Training of Trainers for Gender Mainstreaming with Intersectional Approaches in Counter-Terrorism and Countering/Preventing Violent Extremism",
+    publishedAt: new Date("2024-12-10T00:00:00Z"),
+    updatedAt: new Date("2024-12-10T00:00:00Z"),
   },
   {
-    id: "4",
-    data: { description: "RFP for cybersecurity services" },
+    id: "67113d45d5d021d296315913",
+    data: {
+      description:
+        "ABAAD seeks a consultant to develop a Gender Analysis Guidance Manual to enhance gender-responsive program design specific to Lebanon’s socio-political context. The manual should offer step-by-step guidance o...",
+    },
     userId: null,
-    createdAt: new Date("2023-04-01T10:00:00Z"),
-    title: "Cybersecurity RFP",
-    publishedAt: new Date("2023-04-02T10:00:00Z"),
-    updatedAt: new Date("2023-04-03T10:00:00Z"),
+    createdAt: new Date("2024-12-02T00:00:00Z"),
+    title: "Consultant for Gender Analysis Guidance Manual",
+    publishedAt: new Date("2024-12-02T00:00:00Z"),
+    updatedAt: new Date("2024-12-02T00:00:00Z"),
   },
   {
-    id: "5",
-    data: { description: "RFP for data analytics services" },
+    id: "67113ccbd5d021d296315908",
+    data: {
+      description:
+        "The American Bar Association seeks a consultant to develop a training module on digital privacy for Zambia, aimed at enhancing the capacity of lawyers and law students. The module should cover privacy laws, dat...",
+    },
     userId: null,
-    createdAt: new Date("2023-05-01T10:00:00Z"),
-    title: "Data Analytics RFP",
-    publishedAt: new Date("2023-05-02T10:00:00Z"),
-    updatedAt: new Date("2023-05-03T10:00:00Z"),
+    createdAt: new Date("2024-11-01T00:00:00Z"),
+    title: "Digital Privacy Curriculum Consultant – Zambia",
+    publishedAt: new Date("2024-11-01T00:00:00Z"),
+    updatedAt: new Date("2024-11-01T00:00:00Z"),
+  },
+  {
+    id: "67113ca8d5d021d296315900",
+    data: {
+      description:
+        "The Danish Refugee Council is seeking an external consultant to assist in developing the 2025-2027 CCCM Cluster Strategy and a Capacity Building Strategy for Yemen. This includes conducting consultations, refin...",
+    },
+    userId: null,
+    createdAt: new Date("2024-11-01T00:00:00Z"),
+    title:
+      "External Consultant for Strategy Support to CCCM Cluster and YDR Consortium Yemen",
+    publishedAt: new Date("2024-11-01T00:00:00Z"),
+    updatedAt: new Date("2024-11-01T00:00:00Z"),
+  },
+  {
+    id: "67113bffd5d021d2963158f8",
+    data: {
+      description:
+        "BRAC seeks a consultant, preferably a lawyer with expertise in Philippine procurement laws, to review and contextualize its International Procurement Guidelines for compliance with local regulations. The assign...",
+    },
+    userId: null,
+    createdAt: new Date("2024-11-01T00:00:00Z"),
+    title:
+      "Consultant to Review and Contextualize BRAC International Procurement Guidelines",
+    publishedAt: new Date("2024-11-01T00:00:00Z"),
+    updatedAt: new Date("2024-11-01T00:00:00Z"),
+  },
+  {
+    id: "67113b1fd5d021d2963158e8",
+    data: {
+      description:
+        "Peace Winds Japan seeks a consultant to conduct a Knowledge, Attitude, and Practice (KAP) survey for its WASH program in Kakuma and Kalobeyei. The project aims to evaluate current WASH practices, gaps, and outc...",
+    },
+    userId: null,
+    createdAt: new Date("2024-11-01T00:00:00Z"),
+    title:
+      "Consultant to Conduct Knowledge, Attitude, and Practice (KAP) Survey in Kakuma Refugee Camp",
+    publishedAt: new Date("2024-11-01T00:00:00Z"),
+    updatedAt: new Date("2024-11-01T00:00:00Z"),
   },
 ];
 
